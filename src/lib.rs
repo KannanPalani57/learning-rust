8617c31741fd4a1bd4c165a394b6899212fe5767
Kannan <kannanpalani455@gmail.com>
2024-07-22 15:40:56 +0530

    learn refcell and threads

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub mod learning_traits;
 pub mod learning_lifetime;
 pub mod learning_closures;
 pub mod iterators;
+pub mod refcell;
+pub mod thread;
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use learning_rust::learning_traits::{learning_traits};
 use learning_rust::learning_lifetime::{lifetime};
 use learning_rust::learning_closures::{closure};
 use learning_rust::iterators::{learning_iterators};
+use learning_rust::thread::{learning_threads};
 
 fn main() {
     // learning_traits();
@@ -10,5 +11,8 @@ fn main() {
 
     // closure();
 
-    learning_iterators()
+    // learning_iterators()
+
+    learning_threads();
+
 }
