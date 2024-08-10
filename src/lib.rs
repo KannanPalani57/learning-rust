aff9dd9ddd4e2cfa08babe4c6c8132a8966a0c58
kannan <kannanpalani455@gmail.com>
2024-08-10 15:36:33 +0530

    add file

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,7 @@ pub mod learning_traits;
 pub mod learning_lifetime;
 pub mod learning_closures;
 pub mod iterators;
-<<<<<<< HEAD
 pub mod deref_smart_pointers;
 pub mod write_tests;
-=======
 pub mod refcell;
 pub mod thread;
->>>>>>> 8617c31741fd4a1bd4c165a394b6899212fe5767
